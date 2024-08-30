e530c086bf906c75f67a6a15894b0f778e0cc71c
TriHuynh00 <tri.huynh5@rmit.edu.vn>
2024-08-30 08:40:09 +0700

    Modify my name in groupMember1 method

--- java/prereq/GitPractice.java
+++ java/prereq/GitPractice.java
@@ -17,7 +17,7 @@ public class GitPractice {
    }
 
    public static void groupMember1() {
-      // TODO: Group Member 1, place your code here
+      System.out.println("I am Tri");
    }
 
    public static void groupMember2() {
